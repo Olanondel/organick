b73824547accca7a443fe963b005d5dd57304ddd
EZH <ezh@develup.pro>
2023-12-15 15:45:17 +0200

    add scroll-lock plugin

---
+++ plugins/scroll-lock.client.js
@@ -0,0 +1,25 @@
+import scrollLock from 'scroll-lock';
+const {
+  disablePageScroll,
+  enablePageScroll,
+  refillGaps,
+  getPageScrollBarWidth,
+} = scrollLock;
+
+// Fill gap for fixed elements: [data-scroll-lock-fill-gap]
+// Allow scroll for elements: [data-scroll-lock-scrollable]
+
+export default defineNuxtPlugin({
+  parallel: true,
+  setup() {
+    const scrollLock = {
+      lock: disablePageScroll,
+      unlock: enablePageScroll,
+
+      fillGaps: refillGaps,
+      scrollBarWidth: getPageScrollBarWidth(),
+    };
+
+    return { provide: { scrollLock } };
+  },
+});
