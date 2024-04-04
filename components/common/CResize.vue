b552399f089e41eb6d214edd594b940804f6546d
EZH <ezh@develup.pro>
2024-04-04 14:34:16 +0300

    add CResize.vue

---
+++ components/common/CResize.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="resize">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$design-width: 1920;
+$design-height: 1080;
+$default-size: 16;
+$minWidth: 768px;
+$minHeight: 600px;
+
+.resize {
+  font-size: min(
+    calc($default-size * max($minWidth, 100vw) / $design-width),
+    calc($default-size * max($minHeight, 100vh) / $design-height)
+  );
+
+  @include media-breakpoint-down(sm) {
+    font-size: em($default-size);
+  }
+}
+</style>
