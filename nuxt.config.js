c0b495702cf75d37e571500c1fd1e7c4e6b6ffec
ezh <ezh@develup.pro>
2024-06-11 23:41:33 +0300

    add @nuxtjs/i18n, @vee-validate/yup packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -98,6 +98,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/eslint-module',
     '@pinia/nuxt',
+    // '@nuxtjs/i18n',
     // '@nuxtjs/device',
     '@nuxtjs/robots',
     'nuxt-svgo',
