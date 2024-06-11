8778cf3b4a04b0a5591652265c287f1c2b3173e1
ezh <ezh@develup.pro>
2024-06-11 23:40:19 +0300

    prettify error.vue

--- error.vue
+++ error.vue
@@ -1,8 +1,20 @@
 <template>
-  <pre>{{ error }}</pre>
+  <div id="app" class="error">
+    Error
+
+    <NuxtLayout v-slot="{ className }" class="error__layout">
+      <div :class="className">
+        <pre>{{ error }}</pre>
+      </div>
+    </NuxtLayout>
+
+    <ModalsContainer />
+  </div>
 </template>
 
 <script setup>
+import { ModalsContainer } from 'vue-final-modal';
+
 defineProps({
   error: {
     type: Object,
@@ -10,3 +22,16 @@ defineProps({
   },
 });
 </script>
+
+<style scoped lang="scss">
+.error {
+  $parent: &;
+
+  display: flex;
+  flex-direction: column;
+
+  &:deep(#{$parent}__layout) {
+    flex-grow: 1;
+  }
+}
+</style>
