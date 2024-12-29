08f1b1b8058efa84eb27386291f56c725547eff8
opr <opr@develup.pro>
2024-12-29 19:55:17 +0100

    feat: add simple layout, news page, bg image for error page

---
+++ layouts/simple.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="layouts-simple" :class="className">
+    <LayoutHeader />
+
+    <slot class-name="layouts-default__view" />
+
+    <LayoutFooter />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  className: {
+    type: String,
+    default: undefined,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.layouts-simple {
+  $parent: &;
+
+  display: flex;
+  flex-direction: column;
+
+  &:deep(#{$parent}__view) {
+    flex-grow: 1;
+  }
+
+  &__newsletter {
+    margin-bottom: em(117);
+  }
+}
+</style>
---
+++ pages/news.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <section class="pages-news">
+    <h2>News</h2>
+  </section>
+</template>
+
+<style scoped lang="scss"></style>
